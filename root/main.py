3f8fb5a9a5f0565fdf12517c743197827db43f18
OwenW <owenjwatts1@gmail.com>
2026-09-25 13:39:36 -0600

    Started the main structure

--- root/main.py
+++ root/main.py
@@ -1,3 +1,22 @@
 talked_to_goblin = False
 relationship_mended = False
 code = "9149"
+
+areas = ["hallway1", "living_room", "bathroom", "room1", "room2", "room3", "stairwell", "hallway2", "room4", "room5", "painting"]
+area = areas[0]
+
+
+#You might notice my programs have a theme.
+print("""
+Welcome to the Greatest Library Ever!
+
+There are two floors, with 8 rooms, 2 hallways, and one stairwell. 
+Your task is to solve the mystery. The man in the living room can tell you what the mystery is and give you hints.
+
+""")
+while True:
+    if area == areas[0]:
+        print("""
+        YOU DIED.
+        """)
+
